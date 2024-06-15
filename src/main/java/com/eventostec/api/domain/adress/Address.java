54bca70b441fefe300d55915d226ed2c2c2e19b5
vitorrmarcelino <vitorrmarcelino09@gmail.com>
2024-06-15 10:05:53 -0300

    feat: create entities

---
+++ src/main/java/com/eventostec/api/domain/adress/Address.java
@@ -0,0 +1,30 @@
+package com.eventostec.api.domain.adress;
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
